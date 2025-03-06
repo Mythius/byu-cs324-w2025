857abd2b0c41068e70a2947c1448c2297b46fb8f
Matthias Southwick <south38@ohio.cs.byu.edu>
2025-03-06 14:27:20 -0700

    Checkpoint 1

--- 08-lab-socket-hunt/treasure_hunter.c
+++ 08-lab-socket-hunt/treasure_hunter.c
@@ -1,16 +1,31 @@
 // Replace PUT_USERID_HERE with your actual BYU CS user id, which you can find
 // by running `id -u` on a CS lab machine.
-#define USERID PUT_USERID_HERE
+#define USERID 1823742663
+#define BUFSIZE 8
 
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "sockhelper.h"
+#include <string.h>
 
 int verbose = 0;
 
 void print_bytes(unsigned char *bytes, int byteslen);
 
 int main(int argc, char *argv[]) {
+	char* server = argv[1];
+	unsigned int port = atoi(argv[2]);
+	unsigned short level = atoi(argv[3]);
+	unsigned short seed = atoi(argv[4]);
+
+	unsigned int userid = USERID;
+	unsigned char buf[8];
+	
+	bzero(buf,BUFSIZE);
+	memcpy(&buf[2],&userid,sizeof(int)); // 4 bytes
+	buf[1] = level;
+	memcpy(&buf[6],&seed,sizeof(short)); // 4 bytes
+	print_bytes(buf,8);
 }
 
 void print_bytes(unsigned char *bytes, int byteslen) {
