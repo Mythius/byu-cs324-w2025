e3155d84e732aaea657e4f5c4310b7d0e6a3af88
Matthias Southwick <south38@idaho.cs.byu.edu>
2025-03-25 13:36:09 -0600

    All Test Cases Passed

--- 10-lab-proxy-threadpool/proxy.c
+++ 10-lab-proxy-threadpool/proxy.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <pthread.h>
 #include "sockhelper.h"
 #include <string.h>
 #include <stdlib.h>
@@ -8,9 +8,13 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include "sbuf.h"
+
 
 /* Recommended max object size */
 #define MAX_OBJECT_SIZE 102400
+#define SBUFSIZE  5
+#define NTHREADS  8
 
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0";
 
@@ -18,8 +22,10 @@ int complete_request_received(char *);
 void parse_request(char *, char *, char *, char *, char *);
 void test_parser();
 void print_bytes(unsigned char *, int);
-void handle_client(int);
+void* handle_client(void*);
 int open_sfd( int );
+
+sbuf_t sbuf;
 
 int main(int argc, char *argv[])
 {
@@ -27,13 +33,19 @@ int main(int argc, char *argv[])
 	char *last_arg = argv[argc - 1];
     int port = atoi(last_arg); 
 
+	sbuf_init(&sbuf,SBUFSIZE);
+	pthread_t tid;
+	for (int i = 0; i < NTHREADS; i++) {
+		pthread_create(&tid, NULL, handle_client, NULL);
+	}
+
 	int sfd = open_sfd(port);
 	while(1){
 		struct sockaddr_storage local_addr_ss;
 		struct sockaddr *local_addr = (struct sockaddr *)&local_addr_ss;
 		socklen_t addr_len = sizeof(struct sockaddr_storage);
-		int client = accept(sfd, local_addr, &addr_len);
-		handle_client(client);
+		int client = accept(sfd, local_addr, &addr_len);        
+		sbuf_insert(&sbuf,client);
 	}
 }
 
@@ -204,154 +216,172 @@ int open_sfd( int port ){
 
 }
 
-void handle_client(int client){
-	char buf[1024];
-	int writeto = 0;
-	while(!complete_request_received(buf)){
-		int r = read(client,&buf[writeto],1024);
-		writeto += r;
-		if(r == 0){
-			printf("Client Disconnected\n");
-			return;
+void* handle_client(void *arg){
+	pthread_detach(pthread_self());
+	while(1){
+	
+		int client = sbuf_remove(&sbuf);
+		
+		char buf[1024];
+		int writeto = 0;
+		while(!complete_request_received(buf)){
+			int r = read(client,&buf[writeto],1024);
+			writeto += r;
+			if(r == 0){
+				printf("Client Disconnected\n");
+				return;
+			}
 		}
-	}
-	
-	// print_bytes((unsigned char *)buf,strlen(buf));
+		
+		// print_bytes((unsigned char *)buf,strlen(buf));
 
-	
-	char method[16], hostname[64], port[8], path[64];
+		
+		char method[16], hostname[64], port[8], path[64];
 
-	
-	if (complete_request_received(buf)) {
-		printf("REQUEST COMPLETE\n");
-		parse_request(buf, method, hostname, port, path);
-		printf("METHOD: %s\n", method);
-		printf("HOSTNAME: %s\n", hostname);
-		printf("PORT: %s\n", port);
-		printf("PATH: %s\n", path);
-	} else {
-		printf("REQUEST INCOMPLETE\n");
-	}
-
-	char newheaders[1024];
-	writeto = 0;
-	writeto += sprintf(&newheaders[writeto],"%s http://",method);
-	
-	if( strcmp(port,"80") == 0){
-		writeto += sprintf(&newheaders[writeto],"%s",hostname);
-	} else {
-		writeto += sprintf(&newheaders[writeto],"%s:%s",hostname,port);
-	}
-
-	writeto += sprintf(&newheaders[writeto],"%s HTTP/1.0\r\n",path);
-	
-	if( strcmp(port,"80") == 0){
-		writeto += sprintf(&newheaders[writeto],"Host: %s\r\n",hostname);
-	} else {
-		writeto += sprintf(&newheaders[writeto],"Host: %s:%s\r\n",hostname,port);
-	}
-
-	writeto += sprintf(&newheaders[writeto],"%s\r\n",user_agent_hdr);
-	writeto += sprintf(&newheaders[writeto],"%s\r\n","Connection: close");
-	writeto += sprintf(&newheaders[writeto],"%s\r\n","Proxy-Connection: close");
-	writeto += sprintf(&newheaders[writeto],"\r\n");
-
-	// print_bytes((unsigned char *) newheaders,writeto);
-
-	// fprintf(stderr,"Looking Up Server\n");
-	
-	struct addrinfo hints;
-	struct addrinfo *result, *rp;
-	int sfd, s;
-	
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-	hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
-	// hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
-	hints.ai_protocol = 0;          /* Any protocol */
-
-	s = getaddrinfo(hostname, port, &hints, &result);
-	if (s != 0) {
-		// fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-		exit(EXIT_FAILURE);
-	}
-
-	for (rp = result; rp != NULL; rp = rp->ai_next) {
-		sfd = socket(rp->ai_family, rp->ai_socktype,
-				rp->ai_protocol);
-		if (sfd == -1)
-			continue;
-
-		char ip_str[INET6_ADDRSTRLEN];
-		void *addr;
-
-		if (rp->ai_family == AF_INET) {
-			struct sockaddr_in *ipv4 = (struct sockaddr_in *)rp->ai_addr;
-			addr = &(ipv4->sin_addr);
-		} else if (rp->ai_family == AF_INET6) {
-			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)rp->ai_addr;
-			addr = &(ipv6->sin6_addr);
+		
+		if (complete_request_received(buf)) {
+			// printf("REQUEST COMPLETE\n");
+			parse_request(buf, method, hostname, port, path);
+			// printf("METHOD: %s\n", method);
+			// printf("HOSTNAME: %s\n", hostname);
+			// printf("PORT: %s\n", port);
+			// printf("PATH: %s\n", path);
+		} else {
+			// printf("REQUEST INCOMPLETE\n");
 		}
-	
-		// Convert the binary address to a IP address
-		inet_ntop(rp->ai_family, addr, ip_str, sizeof(ip_str));
-	
-		// Print the hostname, port, and resolved IP
-		// fprintf(stderr, "Resolved hostname: %s, port: %s, IP address: %s\n", hostname, port, ip_str);
-	
-	
-			
-		// if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0){
-			// fprintf(stderr, "Bound\n");
-			break;  
+
+		char newheaders[1024];
+		writeto = 0;
+		writeto += sprintf(&newheaders[writeto],"%s ",method);
+		
+		// if( strcmp(port,"80") == 0){
+		// 	writeto += sprintf(&newheaders[writeto],"%s",hostname);
+		// } else {
+		// 	writeto += sprintf(&newheaders[writeto],"%s:%s",hostname,port);
 		// }
 
+		writeto += sprintf(&newheaders[writeto],"%s HTTP/1.0\r\n",path);
+		
+		if( strcmp(port,"80") == 0){
+			writeto += sprintf(&newheaders[writeto],"Host: %s\r\n",hostname);
+		} else {
+			writeto += sprintf(&newheaders[writeto],"Host: %s:%s\r\n",hostname,port);
+		}
+
+		writeto += sprintf(&newheaders[writeto],"%s\r\n",user_agent_hdr);
+		writeto += sprintf(&newheaders[writeto],"%s\r\n","Connection: close");
+		writeto += sprintf(&newheaders[writeto],"%s\r\n","Proxy-Connection: close");
+		writeto += sprintf(&newheaders[writeto],"\r\n");
+
+		print_bytes((unsigned char *) newheaders,writeto);
+
+		// fprintf(stderr,"Looking Up Server\n");
+		
+		struct addrinfo hints;
+		struct addrinfo *result, *rp;
+		int sfd, s;
+		
+		memset(&hints, 0, sizeof(hints));
+		hints.ai_family = AF_INET;    /* Allow IPv4 or IPv6 */
+		hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+		// hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+		hints.ai_protocol = 0;          /* Any protocol */
+
+		s = getaddrinfo(hostname, port, &hints, &result);
+		if (s != 0) {
+			fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+			exit(EXIT_FAILURE);
+		}
+
+		for (rp = result; rp != NULL; rp = rp->ai_next) {
+			sfd = socket(rp->ai_family, rp->ai_socktype,
+					rp->ai_protocol);
+			if (sfd == -1)
+				continue;
+
+			char ip_str[INET6_ADDRSTRLEN];
+			void *addr;
+
+			if (rp->ai_family == AF_INET) {
+				struct sockaddr_in *ipv4 = (struct sockaddr_in *)rp->ai_addr;
+				addr = &(ipv4->sin_addr);
+			} else if (rp->ai_family == AF_INET6) {
+				struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)rp->ai_addr;
+				addr = &(ipv6->sin6_addr);
+			}
+		
+			// Convert the binary address to a IP address
+			inet_ntop(rp->ai_family, addr, ip_str, sizeof(ip_str));
+		
+			// Print the hostname, port, and resolved IP
+			// fprintf(stderr, "Resolved hostname: %s, port: %s, IP address: %s\n", hostname, port, ip_str);
+		
+		
+				
+			// if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0){
+				// fprintf(stderr, "Bound\n");
+				break;  
+			// }
 
 
-		close(sfd);
-	}
 
-	if (rp == NULL) {
-		// fprintf(stderr, "Error: No valid address found for connection\n");
-		freeaddrinfo(result); // Safe to free if loop fails
-		exit(EXIT_FAILURE);
-	}
+			close(sfd);
+		}
 
-	if(connect(sfd, rp->ai_addr, rp->ai_addrlen) == -1){
-		// fprintf(stderr,"Error Connecting to Server\n");
-	}
+		if (rp == NULL) {
+			fprintf(stderr, "Error: No valid address found for connection\n");
+			freeaddrinfo(result);
+			close(sfd);
+			close(client);
+			continue;
+		}
 
-	
-	if(send(sfd,newheaders,writeto,0) == -1){
-		// fprintf(stderr,"Error Connecting to Send Data\n");
-	}
-	
-	// fprintf(stderr,"Sent Data to Server\n");
-	
-	char response_buf[1024];
-	writeto = 0;
+		if(connect(sfd, rp->ai_addr, rp->ai_addrlen) == -1){
+			fprintf(stderr,"Error Connecting to Server\n");
+			close(sfd);
+			close(client);
+			continue;
+		}
 
-	int n;
-	n = read(sfd,&response_buf[writeto], 1024);
-	writeto += n;
-	while(n != 0){
-		n = read(sfd,&response_buf[writeto], 1024);
+		
+		if(send(sfd,newheaders,writeto,0) == -1){
+			fprintf(stderr,"Error Connecting to Send Data\n");
+			close(sfd);
+			close(client);
+			continue;
+		}
+		
+		// fprintf(stderr,"Sent Data to Server\n");
+		
+		char response_buf[16384];
+		writeto = 0;
+
+		int n;
+		n = read(sfd,&response_buf[writeto], 16384);
 		writeto += n;
+		while(n != 0){
+			n = read(sfd,&response_buf[writeto], 16384);
+			writeto += n;
+		}
+
+		
+		// fprintf(stderr,"Recieved Data from Server\n");
+
+		// print_bytes((unsigned char *) response_buf,writeto);
+
+		if(write(client,response_buf,writeto) == -1){
+			fprintf(stderr,"Error writing response to client\n");
+			close(sfd);
+			close(client);
+			continue;
+		}
+		
+		// fprintf(stderr,"Sent Server data to client\n");
+
+
+		freeaddrinfo(result);
+		close(sfd);
+		close(client);
+			
 	}
-
-	
-	// fprintf(stderr,"Recieved Data from Server\n");
-
-	print_bytes((unsigned char *) response_buf,writeto);
-
-	if(write(client,response_buf,writeto) == -1){
-		// fprintf(stderr,"Error writing response to client\n");
-	}
-	
-	// fprintf(stderr,"Sent Server data to client\n");
-
-
-	freeaddrinfo(result);
-	close(sfd);
-	close(client);
 }
